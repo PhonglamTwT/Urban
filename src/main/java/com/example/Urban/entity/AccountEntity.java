5ef17c551dccd5946cff22eb427f66f6bbae875e
PhonglamTwT <ivyom89@gmail.com>
2024-04-19 15:59:53 +0700

    Initial commit

---
+++ src/main/java/com/example/Urban/entity/AccountEntity.java
@@ -0,0 +1,26 @@
+package com.example.Urban.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity(name="account")
+public class AccountEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private String role;
+
+    @OneToOne
+    @JoinColumn(name = "employee_id")
+    private EmployeeEntity employee;
+
+}
---
+++ src/main/java/com/example/Urban/entity/EmployeeEntity.java
@@ -0,0 +1,31 @@
+package com.example.Urban.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.List;
+
+@Data
+@Entity(name="employee")
+public class EmployeeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @Column(name="name")
+    String name;
+
+    @Column(name="position")
+    String position;
+
+    @Column(name="headquarter")
+    String headquarter;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "employee")
+    private List<EventEntity> events;
+
+    @OneToOne(mappedBy = "employee")
+    private AccountEntity account;
+}

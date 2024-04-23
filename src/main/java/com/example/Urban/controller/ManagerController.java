a9cc5d5e69c4f68bac671c6223756b04558e6483
PhonglamTwT <ivyom89@gmail.com>
2024-04-23 15:56:47 +0700

    ManagerController

---
+++ src/main/java/com/example/Urban/controller/ManagerController.java
@@ -0,0 +1,13 @@
+package com.example.Urban.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/manager")
+public class ManagerController {
+    @PostMapping("/insertEmployee")
+    public ResponseEntity<?> insertEmployee(@RequestParam String name, @RequestParam String headquarter, @RequestParam String position, @RequestParam String password, @RequestParam String role, @RequestParam String username){
+
+    }
+}
---
+++ src/main/java/com/example/Urban/dto/EmployeeDTO.java
@@ -0,0 +1,11 @@
+package com.example.Urban.dto;
+
+import lombok.Data;
+
+@Data
+public class EmployeeDTO {
+    private int id;
+    private String headquarter;
+    private String name;
+    private String position;
+}

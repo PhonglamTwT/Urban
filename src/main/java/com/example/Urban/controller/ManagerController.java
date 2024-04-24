bea4de8cd4d47785bd8b5209f50535210188abf8
PhonglamTwT <ivyom89@gmail.com>
2024-04-24 16:58:46 +0700

    InsertEmployee

--- src/main/java/com/example/Urban/controller/ManagerController.java
+++ src/main/java/com/example/Urban/controller/ManagerController.java
@@ -1,21 +1,32 @@
 package com.example.Urban.controller;
 
+import com.example.Urban.dto.EmployeeAccountDTO;
+import com.example.Urban.service.ManagerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/manager")
 public class ManagerController {
-    @PostMapping("/insertEmployee")
-    public ResponseEntity<?> insertEmployee(@RequestParam String name, @RequestParam String headquarter, @RequestParam String position, @RequestParam String password, @RequestParam String role, @RequestParam String username){
-        System.out.println("Check: "+name+" "+headquarter+" "+position+" "+password+" "+role+" "+username);
-        String message = "";
+    @Autowired
+    private ManagerService managerService;
+
+    @PostMapping("/updateEmployee")
+    public ResponseEntity<?> updateEmployee(@RequestParam int employeeId, @RequestBody EmployeeAccountDTO employeeAccountDTO){
         try{
-            return new ResponseEntity<>("Upload thanh cong", HttpStatus.OK);
+            if(managerService.updateEmployee(employeeId, employeeAccountDTO)){
+                return new ResponseEntity<>("Success", HttpStatus.OK);
+            }
+            else{
+                return new ResponseEntity<>("Fail",HttpStatus.OK);
+            }
+
         }catch (Exception e) {
-            return new ResponseEntity<>("Upload that bai", HttpStatus.OK);
+            return new ResponseEntity<>("Fail", HttpStatus.OK);
         }
     }
 }

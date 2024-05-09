e5510047dc6c8dcad4e75645e0ee2fa477b1102e
ducti <ducti@CHI-DUC>
2024-05-09 16:42:49 +0700

    hehe

--- src/main/java/com/example/Urban/UrbanApplication.java
+++ src/main/java/com/example/Urban/UrbanApplication.java
@@ -1,15 +1,25 @@
 package com.example.Urban;
 
 
+import com.example.Urban.service.FileStorageService;
+import jakarta.annotation.Resource;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class UrbanApplication {
+//@ComponentScan(basePackages = {"com.example.Urban.service", "com.example.Urban.controller"})
+public class UrbanApplication implements CommandLineRunner {
+	@Resource
+	FileStorageService fileStorageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UrbanApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		fileStorageService.init();
+	}
 }
